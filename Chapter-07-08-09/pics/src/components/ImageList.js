715df70e2f7c811c5884712f4f014d3cb214719e
kloudone <arivazhagan@kloudone.com>
2019-06-23 19:36:19 +0530

    Added Styles

--- Chapter-07-08-09/pics/src/components/ImageList.js
+++ Chapter-07-08-09/pics/src/components/ImageList.js
@@ -3,7 +3,10 @@ import React from 'react';
 const ImageList = (props) => {
     console.log(props.images);
     const images = props.images.map(({ description, id, urls }) => {
-        return <img alt={description} key={id} src={urls.regular} />
+        return (<div key={id} className="ui segment">
+            <img className="ui centered medium image" alt={description} src={urls.regular} />
+
+        </div>);
     });
     return <div>{images}</div>
 }
